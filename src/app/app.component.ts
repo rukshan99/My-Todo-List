42df3cfac00dba54680057bca3ea5fc43a387f41
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-04-19 18:22:04 +0530

    crash course completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "Rukshan's Todos";
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.todoValue = "";
+    this.list = [];
+  }
+
+  addTodo() {
+    if(this.todoValue != "") {
+      const newTodo: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newTodo);
+    }
+    this.todoValue = "";
+  }
+
+  deleteTodo(id: number) {
+    this.list = this.list.filter(todo => todo.id!==id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
